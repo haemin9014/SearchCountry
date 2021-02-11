7a06a591fa3da508918d25348e385088eab65f5b
haemin9014 <leehaemin9014@gmail.com>
2021-02-10 20:04:37 -0800

    first change, need error check and style.css

--- script.js
+++ script.js
@@ -8,33 +8,43 @@ const CountryBorders = document.querySelector(".country-borders");
 const quit = document.querySelector("#btn-close");
 const mainCountryAll = document.querySelectorAll(".main-country");
 const bordersCountryAll = document.querySelectorAll(".country-borders");
+const countryP = document.querySelector(".hide-country");
+const borderP = document.querySelector(".hide-border");
 let showMain;
-
-const printMain = function (data) {
-  if (mainCountry) {
-    showMain = `
-  <article class="country">
-    <h1>Country you Searched</h1>
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
+let repeat = 0;
+let hasBorder = false;
+const printMain = function (data, check) {
+  showMain = `
+  <article class="country-${check}">
+    <img class="search-img" src="${data.flag}" />
+    <div class="country-print">
+      <h3 class="country-name">Country Name: ${data.name}</h3>
+      <h4 class="country-region">Region: ${data.region}</h4>
+      <p class="country-population"><span>Population: </span>${(
         +data.population / 1000000
       ).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      <p class="country-lnaguage"><span>Language: </span>${
+        data.languages[0].name
+      }</p>
+      <p class="country-currencies"><span>Currencies: </span>${
+        data.currencies[0].name
+      }</p>
     </div>
   </article>
  `;
-    console.log(`Our show main is : ${showMain}`);
+  if (check === "mainC") {
     mainCountry.insertAdjacentHTML("beforeend", showMain);
-    mainCountry.style.opacity = 1;
-    CountryBorders.style.opacity = 1;
-    quit.style.opacity = 1;
-  } else {
-    console.log("maincountry doesn't exist");
   }
+  if (check === "bordersC") {
+    borderP.style.opacity = 1;
+    CountryBorders.insertAdjacentHTML("beforeend", showMain);
+    CountryBorders.insertAdjacentHTML("beforeend", "  ");
+  }
+
+  countryP.style.opacity = 1;
+  mainCountry.style.opacity = 1;
+  CountryBorders.style.opacity = 1;
+  quit.style.opacity = 1;
 };
 
 const printBorders = async function (data) {};
@@ -62,17 +72,20 @@ const printMainCountry = async function (country) {
     `https://restcountries.eu/rest/v2/name/${country}`
   );
   let [data] = await countryData.json();
-  printMain(data);
-  // printCountryBorders(data);
+  printMain(data, "mainC");
+  printCountryBorders(data);
 };
 
 const printCountryBorders = async function (border) {
-  for (let list of border[0].borders) {
+  repeat = border.borders.length;
+  console.log(repeat);
+  for (let list of border.borders) {
     console.log(list);
     const bordersData = await fetch(
       `https://restcountries.eu/rest/v2/alpha/${list}`
     );
     const data2 = await bordersData.json();
+    printMain(data2, "bordersC");
   }
 };
 
@@ -80,6 +93,12 @@ quit.addEventListener("click", function () {
   quit.style.opacity = 0;
   searchContainer.style.opacity = 1;
   searchBtn.style.opacity = 1;
-  document.querySelector(".country").remove();
+  borderP.style.opacity = 0;
+  countryP.style.opacity = 0;
+  document.querySelector(".country-mainC").remove();
+  for (let i = 0; i < repeat; i++) {
+    document.querySelector(".country-bordersC").remove();
+  }
+  repeat = 0;
   searchBox.value = "Search Country";
 });
